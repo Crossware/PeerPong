5666624f1906eeaacd50ffe1d21623587004224d
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-01 00:30:45 +0100

    TEST the Ball and Paddle tests

--- src/tests/Paddle.test.ts
+++ src/tests/Paddle.test.ts
@@ -1,11 +1,15 @@
 import Paddle from '../classes/Paddle';
 
-let canvas;
-let ctx;
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+const mockFillRectFunction = jest.fn((x: number, y: number, w: number, h: number) => {});
+const mockStrokeFunction = jest.fn();
 
 beforeEach(() => {
   canvas = document.createElement('canvas');
   ctx = canvas.getContext('2d');
+  ctx.fillRect = mockFillRectFunction;
+  ctx.stroke = mockStrokeFunction;
 });
 
 describe('Paddle', () => {
@@ -13,7 +17,7 @@ describe('Paddle', () => {
     var myPaddle = new Paddle(canvas, 10, 10, 0);
     myPaddle.draw();
 
-    expect(ctx.fillStyle).toBe('#eee');
+    expect(ctx.fillStyle).toBe('#eeeeee');
     expect(ctx.fillRect).toHaveBeenCalledTimes(1);
     expect(ctx.fillRect).toHaveBeenCalledWith(10, 10, 10, 100);
     expect(ctx.stroke).toHaveBeenCalledTimes(1);
