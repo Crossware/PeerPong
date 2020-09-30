0656606c7c109cd147b5db77e8eb4e7241003515
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-01 00:29:23 +0100

    ADD a jest config file with no limits

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+};
